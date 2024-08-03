5099537f7dd6cd5f197f76842fea548d9c85293e
iibrahim70 <iibrahiim.dev@gmail.com>
2024-08-03 11:06:16 -0400

    :hammer: update station service

--- src/modules/Station/station.service.ts
+++ src/modules/Station/station.service.ts
@@ -4,13 +4,13 @@ import { IStation } from './station.interface';
 import { Station } from './station.model';
 
 const createStationFromDB = async (payload: IStation) => {
-  // Check if a user with the provided email already exists
+  // Check if a station with the provided stationCode already exists
   if (await Station.isStationExistsByStationCode(payload?.stationCode)) {
     // If user already exists, throw a CONFLICT ApiError
     throw new ApiError(httpStatus.CONFLICT, 'Station already exists!');
   }
 
-  // If user does not exist, create the new station
+  // If station does not exist, create the new station
   const result = await Station.create(payload);
   return result;
 };
@@ -22,6 +22,12 @@ const getAllStationsFromDB = async () => {
 
 const getSingleStationFromDB = async (stationId: string) => {
   const result = await Station.findById(stationId);
+
+  // If no station is found with the provided ID, throw a NOT_FOUND ApiError
+  if (!result) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Station not found with this ID!');
+  }
+
   return result;
 };
 
@@ -29,6 +35,12 @@ const updateStationFromDB = async (stationId: string, payload: IStation) => {
   const result = await Station.findByIdAndUpdate(stationId, payload, {
     new: true,
   });
+
+  // If no station is found with the provided ID, throw a NOT_FOUND ApiError
+  if (!result) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Station not found with this ID!');
+  }
+
   return result;
 };
 
