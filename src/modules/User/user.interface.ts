a35b769dd7304a2787629716fbe6dc3f85c406eb
iibrahim70 <iibrahiim.dev@gmail.com>
2024-08-03 09:33:45 -0400

    :hammer: added user interface

---
+++ src/modules/User/user.interface.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  avatar: string;
+  password: string;
+  wallet: number;
+  role: string;
+  isDeleted: boolean;
+  isBlocked: boolean;
+}
