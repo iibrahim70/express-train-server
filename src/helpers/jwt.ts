c5a3676132707ed35112b2eb1e0b428a3abf2f2a
Ibrahim Khalil <112858983+iibrahim70@users.noreply.github.com>
2024-08-06 01:13:01 +0600

    Update jwt.ts

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,9 +1,5 @@
-/* eslint-disable no-unused-vars */
+import jwt from 'jsonwebtoken';
 
-import jwt, { JwtPayload } from 'jsonwebtoken';
-
-// Define the type for the callback used in verifyToken
-type TCallBack = (err: Error | null, decoded?: JwtPayload) => void;
 
 // Function to create a JWT token
 export const createToken = (
@@ -14,18 +10,5 @@ export const createToken = (
   // Sign the JWT token using jwt.sign
   return jwt.sign(jwtPayload, secret, {
     expiresIn,
-  });
-};
-
-// Function to verify a JWT token
-export const verifyToken = (token: string, secret: string, cb: TCallBack) => {
-  // Verify the JWT token using jwt.verify
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err) {
-      // If an error occurs during verification, pass it to the callback
-      return cb(err);
-    }
-    // If verification succeeds, pass the decoded payload to the callback
-    cb(null, decoded as JwtPayload);
   });
 };
